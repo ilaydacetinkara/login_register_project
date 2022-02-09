742e60c94490dfc82bf7fdf1b5e5ad5e15db7708
ilaydacetinkara <70171880+ilaydacetinkara@users.noreply.github.com>
2022-02-09 20:15:08 +0300

    login register project

---
+++ flutter_application_6/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_6/Screens/Welcome/Welcome_screen.dart';
+import 'package:flutter_application_6/constants.dart';
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Auth',
+      theme: ThemeData(
+        primaryColor: kPrimaryColor,
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      home: WelcomeScreen(),
+    );
+  }
+}
